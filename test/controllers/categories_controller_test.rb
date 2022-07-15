8fe71097fe0f93d8b1528112f75ed8b670b41f83
arielmestrada <estrada.ariel.m@gmail.com>
2022-07-15 20:46:55 +0800

    test complete for category controller

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,22 +1,6 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-=begin
-    test "should get index" do
-        get categories_path
-        assert_response :success
-    end
-
-    test "should show category" do
-        get categories_path(1)
-        assert_response :success
-    end
-
-    test "should create a category" do
-        post categories_path
-        assert_response :success
-    end
-=end
 
     test "should get category index" do 
         get categories_path
@@ -35,13 +19,41 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "should create a category" do
-
         assert_difference("Category.count", 1) do            
             post categories_path, params: { category: { name: "Test" } }
+            assert_response :redirect
         end
         
-    end    
+        follow_redirect!
+        assert_response :success
+    end   
     
+    test "should get edit category" do
+        get edit_category_path(Category.first.id)
+        assert_response :success
+    end
+
+    test "should update category" do
+        @category = Category.create(name: "Some Category")
+
+        patch category_path(@category.id), params: { category: { name: "Edited Name" } }
+
+        @category.reload
+
+        assert_equal "Edited Name", @category.name
+    end
+
+    test "should delete a category" do
+        @category = Category.first
+
+        assert_difference("Category.count", -1) do            
+            delete category_path(@category.id)
+            assert_response :redirect
+        end
+        
+        follow_redirect!
+        assert_response :success
+    end        
 end
 
 
